12d22b326aee06f220806ff7a4698c7d1465ff36
CamiloBosa <camiloandresbosa@gmail.com>
2023-07-03 02:45:30 -0500

    Login

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 var cuentas = [
-    {nombre : "Mali", saldo:200},
-    {nombre : "Guera", saldo:290},
-    {nombre : "Maui", saldo:67}
+    {nombre :  "Mali", password:"1234",saldo:200},
+    {nombre : "Guera",password:"h456", saldo:290},
+    {nombre : "Maui", password:"5678", saldo:67 }/*retiro:function(){},deposito(){}*/
 ]
+var entrar = false;
+//Acceso:
+const clickLogin = () =>{
+    var nombre = document.getElementById("nombre").value;
+    var password = document.getElementById("password").value;
+    contrasena = cuentas[nombre].password
+    if (password == contrasena){
+        console.log("contraseña correcta")
+        entrar = true;
+    }else{
+        console.log("contraseña incorrecta, no puedes ingresar")
+        entrar =  false; 
+    }
+} 
 
-
-
+const login = document.getElementById('boton');
+login.addEventListener("click",clickLogin); 
 
