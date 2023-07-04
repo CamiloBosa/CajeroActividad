5f34105ccd38948e45aec965ba1bc091e3104a49
CamiloBosa <camiloandresbosa@gmail.com>
2023-07-03 22:09:02 -0500

    avance ingreso y operaciones

--- script.js
+++ script.js
@@ -3,18 +3,25 @@ var cuentas = [
     {nombre : "Guera",password:"h456", saldo:290},
     {nombre : "Maui", password:"5678", saldo:67 }/*retiro:function(){},deposito(){}*/
 ]
+
+document.getElementById("formularioDatos").style.display ="none";
+
 var entrar = false;
 //Acceso:
 const clickLogin = () =>{
     var nombre = document.getElementById("nombre").value;
     var password = document.getElementById("password").value;
-    contrasena = cuentas[nombre].password
+    contrasena = cuentas[nombre].password;
+
     if (password == contrasena){
-        console.log("contraseña correcta")
+        console.log("contraseña correcta");
         entrar = true;
+        document.getElementById("App").style.display ="none";
+        document.getElementById("formularioDatos").style.display ="block";
     }else{
         console.log("contraseña incorrecta, no puedes ingresar")
-        entrar =  false; 
+        entrar =  false;
+        alert("contraseña incorrecta")
     }
 } 
 
