6027795cd8b014ff79cfd1234ac3ed0dc8f759ec
CamiloBosa <camiloandresbosa@gmail.com>
2023-07-13 23:48:45 -0500

    Regla de negocio

--- script.js
+++ script.js
@@ -14,6 +14,9 @@ let cuentas = [{
     saldo:290,
     deposit(valor){
         this.saldo = this.saldo + parseInt(valor)
+    },
+    retira(valor){
+        this.saldo = this.saldo - parseInt(valor)
     }
     },
     {nombre : "Maui",
@@ -21,6 +24,8 @@ let cuentas = [{
     saldo:67 ,
     deposit(valor){
         this.saldo = this.saldo + parseInt(valor)
+    },retira(valor){
+        this.saldo = this.saldo - parseInt(valor)
     }}
 ]
 
@@ -57,15 +62,30 @@ function leer(){
 //    console.log(accion)
     if (accion == 1){//Consultar saldo
         saldoMuestra = cuentas[nombre].saldo
-        alert("Tu saldo es "+saldoMuestra)
+        alert("Tu saldo es $"+saldoMuestra)
     }
     if (accion == 2){//Ingresar monto
         ingresando = prompt("Por favor digitar el valor del monto a ingresar")
-        cuentas[nombre].deposit(ingresando)
+        alerta = parseInt(ingresando) + cuentas[nombre].saldo
+        if (alerta>990){
+            alert("El valor ingresado no es valido recuerda que la cuenta no puede exceder $990") 
+        }  
+        else{
+            cuentas[nombre].deposit(ingresando)
+            alert("valor ingresado correctamente, se ingresaron $"+ingresando+ " ahora tu saldo es $" +cuentas[nombre].saldo)
+        }
+             
     }
-    if (accion == 3){//Ingresar monto
+    if (accion == 3){//Retirar monto
         retirando = prompt("Por favor digitar el valor del monto a retirar")
-        cuentas[nombre].retira(retirando)
+        alerta =  cuentas[nombre].saldo - parseInt(retirando)
+        if (alerta<10){
+            alert("El valor a retirar no es valido, recuerda que la cuenta no puede tener menos de $10") 
+        }  
+        else{
+            cuentas[nombre].retira(retirando)
+            alert("valor retirado correctamente, se hizo un retiro por $"+ retirando+ " ahora tu saldo es $" +cuentas[nombre].saldo)
+        }
     }
 
 }
